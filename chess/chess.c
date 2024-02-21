5c005c1cc270e5ee8d4807d4673b825ebac0099b
Adriaan Jacquet <adriaanjacquet@gmail.com>
2024-02-21 09:46:36 +0100

    added possible castle move, not yet properly implementede

--- chess/chess.c
+++ chess/chess.c
@@ -23,85 +23,92 @@ typedef struct _Board {
     __uint64_t bitboards[12];
 } Board;
 
+typedef struct _Game {
+    __uint64_t bitboards[12];
+    bool white_to_play;
+    bool white_castle;
+    bool black_castle;
+} Game;
+
 __uint64_t bit_at_pos(int x, int y) {
     if (x < 0 || y < 0 || x > 7 || y > 7) return 0;
     __uint64_t bit = 0b1000000000000000000000000000000000000000000000000000000000000000;
     return bit >> (x + y*8);
 }
 
-int piece_at_tile(bool white_to_play, Board* board, int x, int y, PIECE piece) {
+int piece_at_tile(Game* game, int x, int y, PIECE piece) {
     if (piece == NOPIECE) {
-        if (white_to_play) {
+        if (game->white_to_play) {
             for (int i = 0; i < 6; i++) {
-                if (((board->bitboards[i] >> (x + y*8)) & 1) != 0) return i;
+                if ((game->bitboards[i] & bit_at_pos(x, y)) != 0) return i; // exact value doesn't matter as long as it's positive
             }
         } else {
             for (int i = 6; i < 12; i++) {
-                if (((board->bitboards[i] >> (x + y*8)) & 1) != 0) return i;
+                if ((game->bitboards[i] & bit_at_pos(x, y)) != 0) return i;
             }
         }
     } else {
-        printf("Checking bitboard: %lu\n", board->bitboards[piece]);
-        printf("Piece at place? %lu\n", board->bitboards[piece] >> (x + y*8));
+        printf("Checking bitboard: %lu\n", game->bitboards[piece]);
+        printf("Piece at place? %lu\n", game->bitboards[piece] >> (x + y*8));
         printf("For piece: %d\n\n", piece);
-        if ((board->bitboards[piece] & (bit_at_pos(x, y))) != 0) return piece;
+        if ((game->bitboards[piece] & (bit_at_pos(x, y))) != 0) return piece;
     }
 
     return -1;
 }
 
-__uint64_t get_white_bitboard(Board* board) {
+__uint64_t get_white_bitboard(Game* game) {
     __uint64_t res = 0;
     for (int i = 0; i <= 5; i++) {
-        res |= board->bitboards[i];
+        res |= game->bitboards[i];
     }
 
     return res;
 }
 
-__uint64_t get_black_bitboard(Board* board) {
+__uint64_t get_black_bitboard(Game* game) {
     __uint64_t res = 0;
     for (int i = 6; i < 12; i++) {
-        res |= board->bitboards[i];
+        res |= game->bitboards[i];
     }
 
     return res;
 }
 
-__uint64_t get_board_bitboard(Board* board) {
+__uint64_t get_board_bitboard(Game* game) {
     __uint64_t res = 0;
     for (int i = 0; i < 12; i++) {
-        res |= board->bitboards[i];
+        res |= game->bitboards[i];
     }
 
     return res;
 }
 
-__uint64_t get_white_pawn_moves(Board* board, int x, int y) {
-    __uint64_t full_board = get_board_bitboard(board);
+__uint64_t get_white_pawn_moves(Game* game, int x, int y) {
+    __uint64_t full_board = get_board_bitboard(game);
     __uint64_t res = 0;
     res |= bit_at_pos(x, y-1) & (~full_board);
     if (y >= 6 && res != 0) res |= bit_at_pos(x, y-2) & (~full_board); // res!=0 check to see if pawn is blocked
     
-    __uint64_t black_board = get_black_bitboard(board);
+    __uint64_t black_board = get_black_bitboard(game);
     res |= bit_at_pos(x+1, y-1) & (black_board);
     res |= bit_at_pos(x-1, y-1) & (black_board);
     return res;
 }
 
-__uint64_t get_black_pawn_moves(Board* board, int x, int y) {
-    __uint64_t full_board = get_board_bitboard(board);
+__uint64_t get_black_pawn_moves(Game* game, int x, int y) {
+    __uint64_t full_board = get_board_bitboard(game);
     __uint64_t res = 0;
     res |= bit_at_pos(x, y+1) & (~full_board);
     if (y <= 1 && res != 0) res |= bit_at_pos(x, y+2) & (~full_board);
 
-    __uint64_t white_board = get_white_bitboard(board);
+    __uint64_t white_board = get_white_bitboard(game);
     res |= bit_at_pos(x+1, y+1) & (white_board);
     res |= bit_at_pos(x-1, y+1) & (white_board);
     return res;
 }
 
-__uint64_t get_white_king_moves(Board* board, int x, int y) {
+__uint64_t get_white_king_moves(Game* game, int x, int y) {
     __uint64_t res = 0;
     for (int j = y-1; j <= y+1; j++) {
         for (int i = x-1; i <= x+1; i++) {
@@ -111,18 +118,19 @@ __uint64_t get_white_king_moves(Board* board, int x, int y) {
         }
     }
 
-    printf("x=%d\n", x);
-    printf("y=%d\n", y);
-    printf("Piece at tile? %d\n", piece_at_tile(true, board, 7, 7, WROOK));
-    if (x==4 && y==7 && (piece_at_tile(true, board, 7, 7, WROOK) != NOPIECE)) {
-        res |= bit_at_pos(7, 7);
+    if (game->white_castle) {
+        bool no_bishop = piece_at_tile(game, x + 1, y, NOPIECE) < 0;
+        bool no_knight = piece_at_tile(game, x + 2, y, NOPIECE) < 0;
+        if (no_bishop && no_knight) {
+            res |= bit_at_pos(x+2, y);
+        }
     }
 
     return res;
 }
 
-__uint64_t get_black_king_moves(Board* board, int x, int y) {
-        __uint64_t res = 0;
+__uint64_t get_black_king_moves(Game* game, int x, int y) {
+    __uint64_t res = 0;
     for (int j = y-1; j <= y+1; j++) {
         for (int i = x-1; i <= x+1; i++) {
             if (j >= 0 && i >= 0 && j < 8 && i < 8) {
@@ -131,14 +139,18 @@ __uint64_t get_black_king_moves(Board* board, int x, int y) {
         }
     }
 
-    // if (piece_at_tile(true, board, 7, 7, WROOK)) {
-    //     res |= bit_at_pos(7, 7); // TODO abstracter + check if there are no pieces in between
-    // }
+    if (game->black_castle) {
+        bool no_bishop = piece_at_tile(game, x + 1, y, NOPIECE) < 0;
+        bool no_knight = piece_at_tile(game, x + 2, y, NOPIECE) < 0;
+        if (no_bishop && no_knight) {
+            res |= bit_at_pos(x+2, y);
+        }
+    }
 
     return res;
 }
 
-__uint64_t get_knight_moves(Board* board, int x, int y) {
+__uint64_t get_knight_moves(Game* game, int x, int y) {
     const int X[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
     const int Y[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
 
@@ -150,8 +162,8 @@ __uint64_t get_knight_moves(Board* board, int x, int y) {
     return res;
 }
 
-__uint64_t get_bishop_moves(Board* board, int x, int y) {
-    __uint64_t full_board = get_board_bitboard(board);
+__uint64_t get_bishop_moves(Game* game, int x, int y) {
+    __uint64_t full_board = get_board_bitboard(game);
     __uint64_t res = 0;
     __uint8_t blocked = 0;
     for (int i = 1; i < 7; i++) {
@@ -175,9 +187,9 @@ __uint64_t get_bishop_moves(Board* board, int x, int y) {
     return res;
 }
 
-__uint64_t get_rook_moves(Board* board, int x, int y) {
+__uint64_t get_rook_moves(Game* game, int x, int y) {
     // TODO cleanup same way as bishop moves
-    __uint64_t full_board = get_board_bitboard(board);
+    __uint64_t full_board = get_board_bitboard(game);
     __uint64_t res = 0;
     for (int i = x+1; i < 8; i++) {
         __uint64_t move = bit_at_pos(i, y) & (~full_board);
@@ -220,32 +232,32 @@ __uint64_t get_rook_moves(Board* board, int x, int y) {
     return res ;
 }
 
-__uint64_t get_queen_moves(Board* board, int x, int y) {
-    return (get_rook_moves(board, x, y) | get_bishop_moves(board, x, y));
+__uint64_t get_queen_moves(Game* game, int x, int y) {
+    return (get_rook_moves(game, x, y) | get_bishop_moves(game, x, y));
 }
 
 
-__uint64_t get_possible_moves(Board* board, PIECE piece, int x, int y) {
+__uint64_t get_possible_moves(Game* game, PIECE piece, int x, int y) {
     __uint64_t moves = 0;
-    if (piece==WKING) moves = get_white_king_moves(board, x, y);
-    if (piece==BKING) moves = get_black_king_moves(board, x, y);
-    else if (piece==WQUEEN || piece==BQUEEN) moves = get_queen_moves(board, x, y);
-    else if (piece==WROOK || piece==BROOK) moves = get_rook_moves(board, x, y);
-    else if (piece==WBISHOP || piece==BBISHOP) moves = get_bishop_moves(board, x, y);
-    else if (piece==WKNIGHT || piece==BKNIGHT) moves = get_knight_moves(board, x, y);
-    else if (piece==WPAWN) moves = get_white_pawn_moves(board, x, y);
-    else if (piece==BPAWN) moves = get_black_pawn_moves(board, x, y);
+    if (piece==WKING) moves = get_white_king_moves(game, x, y);
+    if (piece==BKING) moves = get_black_king_moves(game, x, y);
+    else if (piece==WQUEEN || piece==BQUEEN) moves = get_queen_moves(game, x, y);
+    else if (piece==WROOK || piece==BROOK) moves = get_rook_moves(game, x, y);
+    else if (piece==WBISHOP || piece==BBISHOP) moves = get_bishop_moves(game, x, y);
+    else if (piece==WKNIGHT || piece==BKNIGHT) moves = get_knight_moves(game, x, y);
+    else if (piece==WPAWN) moves = get_white_pawn_moves(game, x, y);
+    else if (piece==BPAWN) moves = get_black_pawn_moves(game, x, y);
 
-    if (piece <= 5) return moves & ~(get_white_bitboard(board));
-    else return moves & ~(get_black_bitboard(board));
+    if (piece <= 5) return moves & ~(get_white_bitboard(game));
+    else return moves & ~(get_black_bitboard(game));
 }
 
-void move_piece(Board* board, PIECE piece, int x1, int y1, int x2, int y2) {
-    board->bitboards[piece] = board->bitboards[piece] & (~(bit_at_pos(x1, y1)));
+void move_piece(Game* game, PIECE piece, int x1, int y1, int x2, int y2) {
+    game->bitboards[piece] = game->bitboards[piece] & (~(bit_at_pos(x1, y1)));
     for (int i = 0; i < 12; i++) {
-        board->bitboards[i] = board->bitboards[i] & (~(bit_at_pos(x2, y2)));
+        game->bitboards[i] = game->bitboards[i] & (~(bit_at_pos(x2, y2)));
     }
-    board->bitboards[piece] = board->bitboards[piece] | (bit_at_pos(x2, y2));
+    game->bitboards[piece] = game->bitboards[piece] | (bit_at_pos(x2, y2));
 }
 
 void draw_possible_moves(__uint64_t possible_moves, int tilesize) {
@@ -259,13 +271,13 @@ void draw_possible_moves(__uint64_t possible_moves, int tilesize) {
     }
 }
 
-bool is_possible_move(Board* board, PIECE piece, int x1, int y1, int x2, int y2) {
-    __uint64_t possible_moves = get_possible_moves(board, piece, x1, y1);
+bool is_possible_move(Game* game, PIECE piece, int x1, int y1, int x2, int y2) {
+    __uint64_t possible_moves = get_possible_moves(game, piece, x1, y1);
     return (possible_moves & bit_at_pos(x2, y2)) != 0; 
 }
 
-Board* init_board() {
-    Board* board = (Board*)malloc(sizeof(Board));
+Game* init_game() {
+    Game* game = (Game*)malloc(sizeof(Game));
     
     __uint64_t wKing = 0b0000000000000000000000000000000000000000000000000000000000001000;
     __uint64_t wQueen = 0b0000000000000000000000000000000000000000000000000000000000010000;
@@ -282,31 +294,34 @@ Board* init_board() {
     __uint64_t bPawn = 0b0000000011111111000000000000000000000000000000000000000000000000;
 
 
-    *board = (Board){
+    *game = (Game){
         .bitboards = {0},
+        .white_to_play = true,
+        .white_castle = true,
+        .black_castle = true,
     };
 
-    board->bitboards[WKING] = wKing;
-    board->bitboards[WQUEEN] = wQueen;
-    board->bitboards[WBISHOP] = wBishop;
-    board->bitboards[WKNIGHT] = wKnight;
-    board->bitboards[WROOK] = wRook;
-    board->bitboards[WPAWN] = wPawn;
+    game->bitboards[WKING] = wKing;
+    game->bitboards[WQUEEN] = wQueen;
+    game->bitboards[WBISHOP] = wBishop;
+    game->bitboards[WKNIGHT] = wKnight;
+    game->bitboards[WROOK] = wRook;
+    game->bitboards[WPAWN] = wPawn;
 
-    board->bitboards[BKING] = bKing;
-    board->bitboards[BQUEEN] = bQueen;
-    board->bitboards[BBISHOP] = bBishop;
-    board->bitboards[BKNIGHT] = bKnight;
-    board->bitboards[BROOK] = bRook;
-    board->bitboards[BPAWN] = bPawn;
+    game->bitboards[BKING] = bKing;
+    game->bitboards[BQUEEN] = bQueen;
+    game->bitboards[BBISHOP] = bBishop;
+    game->bitboards[BKNIGHT] = bKnight;
+    game->bitboards[BROOK] = bRook;
+    game->bitboards[BPAWN] = bPawn;
 
-    return board;
+    return game;
 }
 
-void destroy_board(Board** board) {
-    if (*board != NULL) {
-        free(*board);
-        *board = NULL;
+void destroy_board(Game** game) {
+    if (*game != NULL) {
+        free(*game);
+        *game = NULL;
     }
 }
 
@@ -330,9 +345,9 @@ void draw_piece(__uint64_t bitboard, Texture2D* texture, size_t tilesize) {
     }
 }
 
-void draw_pieces(Board* board, Texture2D* textures, size_t tilesize) {
+void draw_pieces(Game* game, Texture2D* textures, size_t tilesize) {
     for (int i = 0; i < 12; i++) {
-        draw_piece(board->bitboards[i], &textures[i], tilesize);
+        draw_piece(game->bitboards[i], &textures[i], tilesize);
     }
 }
 
@@ -378,10 +393,9 @@ int main() {
     InitWindow(800, 800, "Chess");
 
     Texture2D* textures = load_textures(image_paths);
-    Board* board = init_board();
+    Game* game = init_game();
 
     size_t tilesize = 0;
-    bool white_to_play = true;
     int clicked_piece = -1; // -1 for no click, >=0 for bitboard which was clicked
     int clicked_piece_x = 0;
     int clicked_piece_y = 0;
@@ -396,9 +410,9 @@ int main() {
             int dropped_x = GetMouseX() / (boardSize / 8);
             int dropped_y = GetMouseY() / (boardSize / 8);
 
-            if (is_possible_move(board, clicked_piece, clicked_piece_x, clicked_piece_y, dropped_x, dropped_y)) {
-                move_piece(board, clicked_piece, clicked_piece_x, clicked_piece_y, dropped_x, dropped_y);
-                white_to_play = !white_to_play;
+            if (is_possible_move(game, clicked_piece, clicked_piece_x, clicked_piece_y, dropped_x, dropped_y)) {
+                move_piece(game, clicked_piece, clicked_piece_x, clicked_piece_y, dropped_x, dropped_y);
+                game->white_to_play = !game->white_to_play;
             }
             clicked_piece = -1;
         } else if ((clicked_piece == -1) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
@@ -408,14 +422,14 @@ int main() {
             clicked_piece_x = GetMouseX() / (boardSize / 8);
             clicked_piece_y = GetMouseY() / (boardSize / 8);
             
-            clicked_piece = piece_at_tile(white_to_play, board, 7 - clicked_piece_x, 7 - clicked_piece_y, -1);
+            clicked_piece = piece_at_tile(game, clicked_piece_x, clicked_piece_y, -1);
         }
 
         BeginDrawing();
             ClearBackground(BLACK);
             draw_board(tilesize);
-            if (clicked_piece != -1) draw_possible_moves(get_possible_moves(board, clicked_piece, clicked_piece_x, clicked_piece_y), tilesize);
-            draw_pieces(board, textures, tilesize);
+            if (clicked_piece != -1) draw_possible_moves(get_possible_moves(game, clicked_piece, clicked_piece_x, clicked_piece_y), tilesize);
+            draw_pieces(game, textures, tilesize);
         EndDrawing();
 
     }
@@ -426,7 +440,7 @@ int main() {
     free(textures);
     CloseWindow();
 
-    destroy_board(&board);
+    destroy_board(&game);
 
 
     return 0;
